c43fd3dc7b80d982bf2bcd529cc6f880a328f1d7
Emam96 <emamsh990@gmail.com>
2021-08-27 14:31:29 +0300

    Starter

---
+++ src/components/mainPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class mainPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default mainPage;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from "@auth0/auth0-react";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+     <Auth0Provider
+    domain="dev-ydeqq4ws.us.auth0.com"
+    clientId="71jPrz2FRUegLK2DrHVQKIrbmmDt5z6b"
+    redirectUri={window.location.origin}
+  >
     <App />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
